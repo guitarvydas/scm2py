e6fa1a5ce72c592515309b315c1c876a4d34af83
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-05-29 20:58:26 -0400

    scrubber

---
+++ scrubber.js
@@ -0,0 +1,9 @@
+// remove special chars like "❲" and "❳"
+function scrubber (s) {
+   return s.replace (/❲/g, '').replace (/❳/g, '');
+}
+
+let fs = require('fs');
+let inp = fs.readFileSync(0, 'utf-8');
+let outp = scrubber (inp);
+console.log (outp);
