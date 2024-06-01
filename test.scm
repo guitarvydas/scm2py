a3bc8f4087009202e05d97386fb2cb0c08449a32
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-05-31 23:03:44 -0400

    plus

--- test.scm
+++ test.scm
@@ -1,3 +1,19 @@
 (define (call-foreign expr bindings)
+  (let ((func (cadr expr))
+        (args (cddr expr)))
+
+    (cond ((string=? "unity" func)
+           (car args))
+
+          ((string=? "add" func)
+           (let ((resolved-args (resolveArgs args bindings)))
+             (+ (car resolved-args) (cadr resolved-args))))
+
+          ((string=? "display" func)
            (let ((a (value (car args) bindings)))
              (display a)))
+          
+          ((string=? "newline" func)
+           (newline))
+          
+          (else (error "call-foreign called with unknown operator" func)))))
